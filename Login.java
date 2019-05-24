0a4f871d6c090399a074cac0fcccd913334bd27a
Hugo Alvarez <hugoferneya@hotmail.com>
2019-05-23 22:31:43 -0700

    login frame some changes

--- Login.java
+++ Login.java
@@ -10,11 +10,11 @@ import javax.swing.JTextField;
 
 public class Login extends JInternalFrame{
 		
-		JPanel top,mid;
-		JLabel title,email,password;
-		JTextField em;
-		JPasswordField pass;
-		JButton log;
+	JPanel top,mid;
+	JLabel title,email,password;
+	JTextField em;
+	JPasswordField pass;
+	JButton log;
 		
 	public Login() {
 		// TODO Auto-generated constructor stub
@@ -26,20 +26,21 @@ public class Login extends JInternalFrame{
 	}
 
 	private void init() {
+    top = new JPanel(); 
 		title = new JLabel("LOGIN");
 		email = new JLabel("email");
 		password = new JLabel("password");
 		email.setSize(50,50);
 		
-		email.setBounds(0,100,400,200);
-		password.setBounds(0, 300, 100, 100);
-		em = new JTextField();
-		pass = new JPasswordField();
-		add(em);
-		add(pass);
-		setLayout(null);
-		
-		
+		//email.setBounds(0,100,400,200);
+		//password.setBounds(0, 300, 100, 100);
+		em = new JTextField(16);
+		pass = new JPasswordField(16);
+		top.add(email);
+    top.add(em);
+    top.add(password);	
+		top.add(pass);
+    this.add(top);
+		//setLayout(null);
 	}
-
 }
